c2803920e30105ad56661ee30bfb82cbc48bf707
bribed.fyi <contact@bribed.fyi>
2026-08-19 09:53:30 -0500

    Footer: link to public source on GitHub

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,11 +23,12 @@ export function Footer() {
         <Link href="/data">Data</Link>
         <Link href="/terms">Terms</Link>
         <Link href="/privacy">Privacy</Link>
+        <a href="https://github.com/rajyalabs/bribed-fyi" target="_blank" rel="noreferrer">Source</a>
       </nav>
       <div className="foot-icons">
-        <a href="https://twitter.com" aria-label="Twitter / X" target="_blank" rel="noreferrer">
+        <a href="https://github.com/rajyalabs/bribed-fyi" aria-label="Source code on GitHub" target="_blank" rel="noreferrer">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-            <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22" />
           </svg>
         </a>
         <Link aria-label="Data & downloads" href="/data">
